a4208e6c4638483f0e799ae3375bda07c0008a08
Mohamad Arouni <mohamadarouni5@gmail.com>
2025-05-05 15:10:22 +0100

    Updated contact form, and gitignore so that all important confidential files arent commited

--- app/routes/contact.py
+++ app/routes/contact.py
@@ -1,34 +1,10 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
-from app.forms.contact_form import ContactForm
-from app.utils.email import send_email
+from flask import Blueprint, render_template
 from flask_login import current_user
 
 # Create blueprint
 contact_bp = Blueprint('contact', __name__, url_prefix='/contact')
 
-@contact_bp.route('/', methods=['GET', 'POST'])
+@contact_bp.route('/', methods=['GET'])
 def index():
-    """Contact page route"""
-    form = ContactForm()
-    
-    # Pre-fill email if user is logged in
-    if current_user.is_authenticated and not form.email.data:
-        form.email.data = current_user.email
-        form.name.data = f"{current_user.first_name} {current_user.last_name}"
-    
-    if form.validate_on_submit():
-        # Send email to admin
-        admin_email = 'mohamadarouni5@gmail.com'  # Or use from config
-        send_email(
-            to=admin_email,
-            subject=f'Contact Form: {form.subject.data}',
-            template='contact/email/new_message',
-            name=form.name.data,
-            email=form.email.data,
-            message=form.message.data
-        )
-        
-        flash('Your message has been sent successfully! We will get back to you soon.', 'success')
-        return redirect(url_for('contact.index'))
-        
-    return render_template('contact/index.html', form=form) 
+    """Contact page route - Using Formspree for form submission"""
+    return render_template('contact/index.html') 
